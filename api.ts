8743efbc6eac98e02356cb5a7e36e5ef50ff408b
minirenatinho <renatodeyvson@gmail.com>
2025-08-06 19:25:36 -0300

    fix: supermarket list on android

--- api.ts
+++ api.ts
@@ -3,8 +3,8 @@ import { Task } from './types';
 import Constants from 'expo-constants';
 
 // Get the API URL from app.config.js
-const API_BASE = Constants.expoConfig?.extra?.apiUrl || 'http://localhost:8000/api/v1/tasks/';
-const SUPERMARKET_API_BASE = 'http://localhost:8000/api/v1/supermarket/';
+const API_BASE = Constants.expoConfig?.extra?.apiUrl || 'http://192.168.1.14:8000/api/v1/tasks/';
+const SUPERMARKET_API_BASE = 'http://192.168.1.14:8000/api/v1/supermarket/';
 
 export async function fetchTasks(): Promise<Task[]> {
   const res = await fetch(API_BASE);
