eefbe268b1b95a429384d94ac87fdef5e892adff
minirenatinho <renatodeyvson@gmail.com>
2025-09-06 19:18:02 -0300

    refact: types, api and readme.md

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ module.exports = ({ config }) => {
     ...config,
     extra: {
       eas: {
-        projectId: "6c1a19bd-1c52-4b85-b95b-ef07cdf67aad"
+        projectId: process.env.EAS_PROJECT_ID,
       },
       EXPO_API_URL: process.env.EXPO_API_URL,
     },
