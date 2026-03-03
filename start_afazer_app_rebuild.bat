f398b10af4ad16fe5297b5cc16c64e3f168960ae
minirenatinho <renatodeyvson@gmail.com>
2026-03-03 16:49:41 -0300

    chore: updating .bat with normal run and rebuild

--- start_afazer_app_rebuild.bat
+++ start_afazer_app_rebuild.bat
@@ -1,5 +1,13 @@
 @echo off
 
+REM Clear old dist build to avoid version mismatches
+if exist dist (
+    rmdir /s /q dist
+)
+
+REM Export the Expo web app with current dependencies
+npx expo export --platform web
+
 REM Serve the web build from dist directory
 start /b npx serve -s dist -l 3000
 
